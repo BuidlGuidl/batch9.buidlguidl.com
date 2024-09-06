0cb614c142fe2fb919d2bc281bb9d2ba3828c25c
phipsae <philip.krause88@yahoo.de>
2024-09-06 12:27:05 +0200

    Update externalContracts.ts
    Add new BatchRegistry contract address to externalContracts.ts

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -14,7 +14,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const externalContracts = {
   10: {
     BatchRegistry: {
-      address: "0x0A7d97d392e7400D15460ae0C9799951a3719393",
+      address: "0x107d6F280a05f07B59039143CA21e3f917AAFA30",
       abi: [
         {
           inputs: [
