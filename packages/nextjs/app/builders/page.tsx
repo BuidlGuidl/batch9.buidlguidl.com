70eeb0e5fc2cdaba26f86e957143b6e46d97966c
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-09-14 23:07:58 +0200

    Get builders from contract

--- packages/nextjs/app/builders/page.tsx
+++ packages/nextjs/app/builders/page.tsx
@@ -1,10 +1,7 @@
-import type { NextPage } from "next";
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+"use client";
 
-export const metadata = getMetadata({
-  title: "Builders",
-  description: "List of members of the batch #9",
-});
+import type { NextPage } from "next";
+import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
 const getRandomGender = () => {
   return Math.random() < 0.5 ? "men" : "women";
@@ -18,26 +15,48 @@ const getRandomUserImage = () => {
 };
 
 const mentors = [
-  { name: "Mentor 1", imgUrl: getRandomUserImage(), link: "/builders/0x000000" },
-  { name: "Mentor 2", imgUrl: getRandomUserImage(), link: "/builders/0x000000" },
-  { name: "Mentor 3", imgUrl: getRandomUserImage(), link: "/builders/0x000000" },
+  { name: "Eda", imgUrl: "https://avatars.githubusercontent.com/u/22100698?v=4", link: "/builders/0x000000" },
+  { name: "Derrek", imgUrl: "https://avatars.githubusercontent.com/u/80121818?v=4", link: "/builders/0x000000" },
+  { name: "Philip", imgUrl: "https://avatars.githubusercontent.com/u/90064316?v=4", link: "/builders/0x000000" },
 ];
 
-const builders = [
-  { name: "Builder 1", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: true },
-  { name: "Builder 2", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: false },
-  { name: "Builder 3", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: true },
-  { name: "Builder 1", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: true },
-  { name: "Builder 2", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: false },
-  { name: "Builder 3", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: true },
-  { name: "Builder 1", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: true },
-  { name: "Builder 2", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: false },
-  { name: "Builder 3", imgUrl: getRandomUserImage(), link: "/builders/0x000000", checkIn: true },
-];
+type Builder = {
+  image: string;
+  link: string;
+  checkedIn: boolean;
+};
+
+const getBuilderFromEvent = (event: any): Builder => {
+  const args = event.args;
+
+  const person: Builder = {
+    image: getRandomUserImage(),
+    link: "builders/" + args.builder,
+    checkedIn: true,
+  };
+
+  return person;
+};
 
 const Builders: NextPage = () => {
-  const totalBuilders = builders.length;
-  const buildersCheckedIn = builders.filter(builder => builder.checkIn).length;
+  const { data: events } = useScaffoldEventHistory({
+    contractName: "BatchRegistry",
+    eventName: "CheckedIn",
+    fromBlock: 31231n,
+    watch: true,
+    filters: { first: true } as any,
+    blockData: true,
+    transactionData: true,
+    receiptData: true,
+  });
+
+  let buildersCheckedIn = 0;
+  let builders: Builder[] = [];
+
+  if (events != undefined) {
+    buildersCheckedIn = events.length;
+    builders = events.map(e => getBuilderFromEvent(e));
+  }
 
   return (
     <>
@@ -50,8 +69,8 @@ const Builders: NextPage = () => {
           <div className="mb-12">
             <h2 className="text-2xl font-semibold text-center mb-6">Mentors</h2>
             <div className="grid grid-cols-3 gap-6">
-              {mentors.map(mentor => (
-                <div key={mentor.name} className="text-center">
+              {mentors.map((mentor, i) => (
+                <div key={i} className="text-center">
                   <img className="w-24 h-24 rounded-full mx-auto" src={mentor.imgUrl} alt={mentor.name} />
                   <a
                     href={mentor.link}
@@ -68,20 +87,18 @@ const Builders: NextPage = () => {
 
           <div>
             <h2 className="text-2xl font-semibold text-center mb-6">Builders 🏗</h2>
-            <p className="text-sm font-bold text-center mb-8">
-              Builders that checkedIn: {buildersCheckedIn} de {totalBuilders}
-            </p>
+            <p className="text-sm font-bold text-center mb-8">Builders that checkedIn: {buildersCheckedIn}</p>
             <div className="grid grid-cols-5 gap-20">
-              {builders.map(builder => (
-                <div key={builder.name} className="text-center">
-                  <img className="w-24 h-24 rounded-full mx-auto" src={builder.imgUrl} alt={builder.name} />
+              {builders.map((builder, i) => (
+                <div key={i} className="text-center">
                   <a
                     href={builder.link}
                     className="mt-2 block text-blue-600 hover:underline font-medium"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    {builder.name}
+                    <img className="w-24 h-24 rounded-full mx-auto" src={builder.image} alt={builder.link} />
+                    See profile
                   </a>
                 </div>
               ))}
