0f22fd48f14d1a26d19f8d9e571262c321691f9a
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-09-14 22:34:44 +0200

    Moved builders checked in

--- packages/nextjs/app/builders/page.tsx
+++ packages/nextjs/app/builders/page.tsx
@@ -42,14 +42,11 @@ const Builders: NextPage = () => {
   return (
     <>
       <div className="text-center bg-secondary p-10">
-        <h1 className="text-4xl my-0">Builders</h1>
+        <h1 className="text-4xl my-0">Batch #9 people 💻 </h1>
       </div>
+
       <div className="max-w-screen-lg mx-auto mt-10 p-6 bg-base-300 shadow-md rounded-lg">
         <div className="container mx-auto px-4 py-8">
-          <h1 className="text-sm font-bold text-center mb-8">
-            Builders that checkedIn: {buildersCheckedIn} de {totalBuilders}
-          </h1>
-
           <div className="mb-12">
             <h2 className="text-2xl font-semibold text-center mb-6">Mentors</h2>
             <div className="grid grid-cols-3 gap-6">
@@ -71,7 +68,10 @@ const Builders: NextPage = () => {
 
           <div>
             <h2 className="text-2xl font-semibold text-center mb-6">Builders 🏗</h2>
-            <div className="grid grid-cols-6 gap-6">
+            <p className="text-sm font-bold text-center mb-8">
+              Builders that checkedIn: {buildersCheckedIn} de {totalBuilders}
+            </p>
+            <div className="grid grid-cols-5 gap-20">
               {builders.map(builder => (
                 <div key={builder.name} className="text-center">
                   <img className="w-24 h-24 rounded-full mx-auto" src={builder.imgUrl} alt={builder.name} />
